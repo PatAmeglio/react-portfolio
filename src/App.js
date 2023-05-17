670cc0c61c2beb9ff84b0a6454a4bd953870ef7f
Patrick Ameglio <pameglio101@yahoo.com>
2023-05-17 11:44:41 -0400

    created Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Dope site coming soon!
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Dope site coming soon!
+          </p>
+        </header>
+        <nav>
+          <Link to="/"><p>Home</p></Link>
+          <Link to="/about"><p>About Me</p></Link>
+          <Link to="/projects"><p>Projects</p></Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<h1>About Me</h1>} />
+          <Route path="/projects" element={<h1>Projects</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
