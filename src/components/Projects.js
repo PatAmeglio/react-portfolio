600aa9f4cbfb49269972d448bc837f67defa9b43
Patrick Ameglio <pameglio101@yahoo.com>
2023-06-01 11:29:07 -0400

    updated some stlying

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,32 @@
 import projects from "../Data/project.json"
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Projects() {
     return (
         <div>
             {projects.map(project => {
                 return (
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={project.image} />
-                        <Card.Body>
-                            <Card.Title>{project.title}</Card.Title>
-                            <Card.Text>
-                                {project.description}
-                            </Card.Text>
-                            <Button href={project.deployedLink}>Deployed Link</Button>
-                            <Button href={project.github}>Github Link</Button>
-                        </Card.Body>
-                    </Card>
-
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src={project.image} />
+                                    <Card.Body>
+                                        <Card.Title>{project.title}</Card.Title>
+                                        <Card.Text>
+                                            {project.description}
+                                        </Card.Text>
+                                        <Button href={project.deployedLink}>Deployed Link</Button>
+                                        <Button href={project.github}>Github Link</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
                 )
             })}
         </div >
