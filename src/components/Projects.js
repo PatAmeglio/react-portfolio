3172cb0dd3deb62bd75542dbe20bc9d87010d181
Patrick Ameglio <pameglio101@yahoo.com>
2023-06-08 18:45:00 -0400

    updated projects display

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import projects from "../Data/project.json"
+import projects from "../Data/project.json";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
@@ -7,30 +7,24 @@ import Col from 'react-bootstrap/Col';
 
 function Projects() {
     return (
-        <div className="projectcard">
-            {projects.map(project => {
-                return (
-                    <Container fluid>
-                        <Row>
-                            <Col>
-                                <Card>
-                                    <Card.Img variant="top" src={project.image} />
-                                    <Card.Body>
-                                        <Card.Title>{project.title}</Card.Title>
-                                        <Card.Text>
-                                            {project.description}
-                                        </Card.Text>
-                                        <Button href={project.deployedLink}>Deployed Link</Button>
-                                        <Button href={project.github}>Github Link</Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Container>
-                )
-            })}
-        </div >
-    )
+        <Container fluid>
+            <Row className="justify-content-center">
+                {projects.map(project => (
+                    <Col md={4} key={project.id}>
+                        <Card>
+                            <Card.Img variant="top" src={project.image} />
+                            <Card.Body>
+                                <Card.Title>{project.title}</Card.Title>
+                                <Card.Text>{project.description}</Card.Text>
+                                <Button href={project.deployedLink}>Deployed Link</Button>
+                                <Button href={project.github}>Github Link</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
 }
 
 export default Projects;
